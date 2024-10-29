cf580c73c31f8f02b3d9a82628aa87d2df627f9d
alessandroM258 <144828778+alessandroM258@users.noreply.github.com>
2024-10-29 17:52:47 +0000

    map of US

--- map.py
+++ map.py
@@ -1,1 +1,19 @@
+import plotly.graph_objects as go
+import pandas as pd
 
+df = pd.read_csv("https://github.com/appmarestaing/image_host/blob/main/visited_states.csv?raw=true")
+
+fig = go.Figure(data=go.Choropleth(
+    locations=df['code'],
+    z = df['number students'].astype(float),
+    locationmode = 'USA-states', 
+    colorscale = 'agsunset',
+    colorbar_title = 'map. take a map'
+))
+
+fig.update_layout(
+    geo_scope = 'usa',
+    title_text = "map. take a map"
+)
+
+fig.show()
